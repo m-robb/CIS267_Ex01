d5b94627f4b9f0fb7a066c1502cccdb808b897f3
m-robb <58054511+m-robb@users.noreply.github.com>
2023-09-18 16:54:04 -0400

    Jump Fix

--- BasicMovementJump/Assets/Scripts/PlayerMovement.cs
+++ BasicMovementJump/Assets/Scripts/PlayerMovement.cs
@@ -1,57 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
-{
-    //gravity: 5
-    private Rigidbody2D playerRigidBody;
-    //8
-    public float movementSpeed;
-    private float inputHorizontal;
-    //12
-    public float jumpForce;
+public class PlayerMovement : MonoBehaviour {
+	//gravity: 5
+	private Rigidbody2D playerRigidBody;
+	//8
+	public float movementSpeed;
+	private float inputHorizontal;
+	//12
+	public float jumpForce;
+	private bool isGrounded;
+	// Start is called before the first frame update
+	void Start() {
+		//Debug.Log("Start");
+		//I can only get this compenent using the following line of code
+		//becuase the rigidbody2d is attached to the player and this script
+		//is also attached to the player.
+		playerRigidBody = GetComponent<Rigidbody2D>();
+	}
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        //Debug.Log("Start");
-        //I can only get this compenent using the following line of code
-        //becuase the rigidbody2d is attached to the player and this script
-        //is also attached to the player.
-        playerRigidBody = GetComponent<Rigidbody2D>();
-    }
+	// Update is called once per frame
+	void Update() {
+		//Debug.Log("Update");
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log("Update");
+		//Basic movement not ideal
+		//moves an object but will ignore collisions
+		//transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
 
-        //Basic movement not ideal
-        //moves an object but will ignore collisions
-        //transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
+		movePlayerLateral();
+		jump();
+	}
 
-        movePlayerLateral();
-        jump();
-    }
+	private void movePlayerLateral() {
+		//Value returned will be 0, 1, or -1 depending on what button is pressed
+		//no button press: 0
+		//right arrow key or d: 1
+		//left arrow key or a: -1
+		//"Horizontal" is defined in the input section of project settings
+		inputHorizontal = Input.GetAxisRaw("Horizontal");
 
-    private void movePlayerLateral()
-    {
-        //Value returned will be 0, 1, or -1 depending on what button is pressed
-        //no button press: 0
-        //right arrow key or d: 1
-        //left arrow key or a: -1
-        //"Horizontal" is defined in the input section of project settings
-        inputHorizontal = Input.GetAxisRaw("Horizontal");
+		playerRigidBody.velocity = new Vector2(movementSpeed * inputHorizontal, playerRigidBody.velocity.y);
+	}
 
-        playerRigidBody.velocity = new Vector2(movementSpeed * inputHorizontal, playerRigidBody.velocity.y);
-    }
+	private void jump() {
+		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+			playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+		}
+	}
 
-    private void jump()
-    {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
-        }
-    }
+	private void OnCollisionEnter2D(Collision2D collision) {
+		//Debug.Log(collision.gameObject);
+
+		if (collision.gameObject.CompareTag("OB")) {
+			//Need to import SceneManagement
+			SceneManager.LoadScene("SampleScene");
+		}
+		else if (collision.gameObject.CompareTag("Ground")) {
+			isGrounded = true;
+		}
+	}
+
+	private void OnCollisionExit2D(Collision2D collision) {
+		if (collision.gameObject.CompareTag("Ground")) {
+			isGrounded = false;
+		}
+	}
 }
