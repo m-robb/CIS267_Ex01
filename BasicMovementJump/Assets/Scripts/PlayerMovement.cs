f31d599fc06a59e4e53aa146d3891cd49e36ee07
m-robb <58054511+m-robb@users.noreply.github.com>
2023-09-20 16:20:06 -0400

    jump fixes

--- BasicMovementJump/Assets/Scripts/PlayerMovement.cs
+++ BasicMovementJump/Assets/Scripts/PlayerMovement.cs
@@ -11,60 +11,60 @@ public class PlayerMovement : MonoBehaviour {
 	private float inputHorizontal;
 	//12
 	public float jumpForce;
-	private bool isGrounded;
+	private int numJumps;
+	private int maxNumJumps;
 	// Start is called before the first frame update
 	void Start() {
-		//Debug.Log("Start");
-		//I can only get this compenent using the following line of code
-		//becuase the rigidbody2d is attached to the player and this script
-		//is also attached to the player.
 		playerRigidBody = GetComponent<Rigidbody2D>();
+		maxNumJumps = 1;
+		numJumps = 0;
 	}
 
 	// Update is called once per frame
 	void Update() {
-		//Debug.Log("Update");
-
-		//Basic movement not ideal
-		//moves an object but will ignore collisions
-		//transform.Translate(Vector2.right * movementSpeed * Time.deltaTime);
-
 		movePlayerLateral();
 		jump();
 	}
 
 	private void movePlayerLateral() {
-		//Value returned will be 0, 1, or -1 depending on what button is pressed
-		//no button press: 0
-		//right arrow key or d: 1
-		//left arrow key or a: -1
-		//"Horizontal" is defined in the input section of project settings
 		inputHorizontal = Input.GetAxisRaw("Horizontal");
 
 		playerRigidBody.velocity = new Vector2(movementSpeed * inputHorizontal, playerRigidBody.velocity.y);
+
+		flipPlayer();
+	}
+
+	private void flipPlayer() {
+		if (inputHorizontal > 0) {
+			transform.localRotation = Quaternion.Euler(0, 0, 0);
+		}
+		else if (inputHorizontal < 0) {
+			transform.localRotation = Quaternion.Euler(0, 180, 0);
+		}
 	}
 
 	private void jump() {
-		if (Input.GetKeyDown(KeyCode.Space) && isGrounded) {
+		if (Input.GetKeyDown(KeyCode.Space) && numJumps < maxNumJumps) {
 			playerRigidBody.velocity = new Vector2(playerRigidBody.velocity.x, jumpForce);
+
+			numJumps++;
 		}
 	}
 
 	private void OnCollisionEnter2D(Collision2D collision) {
-		//Debug.Log(collision.gameObject);
-
 		if (collision.gameObject.CompareTag("OB")) {
 			//Need to import SceneManagement
 			SceneManager.LoadScene("SampleScene");
 		}
 		else if (collision.gameObject.CompareTag("Ground")) {
-			isGrounded = true;
+			numJumps = 0;
 		}
 	}
 
-	private void OnCollisionExit2D(Collision2D collision) {
-		if (collision.gameObject.CompareTag("Ground")) {
-			isGrounded = false;
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.gameObject.CompareTag("DoubleJump")) {
+			Destroy(collision.gameObject);
+			maxNumJumps = 2;
 		}
 	}
 }
